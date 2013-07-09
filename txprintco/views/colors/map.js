8aa7af9c87e30ce3ba88a2d7dce7013858067877
Usman Tamanna <usman8806>
2013-07-09 00:05:27 -0500

    Updated colors view's map.js

--- txprintco/views/colors/map.js
+++ txprintco/views/colors/map.js
@@ -1,10 +1,12 @@
 function(doc) {
 	for(i in doc.runsizes) {
-		var colors = new Array();
+		//var colors = new Array();
+		var colors = {};
 		for(j in doc.runsizes[i]["colors"]) {
-			var obj = {};
-			obj[j] = doc.runsizes[i]["colors"][j].color;
-			colors.push(obj);
+			//var obj = {};
+			//obj[j] = doc.runsizes[i]["colors"][j].color;
+			//colors.push(obj);
+			colors[j] = doc.runsizes[i]["colors"][j].color;
 		}
 
 		emit([doc._id, i], colors);
