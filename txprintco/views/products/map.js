0b66256b40dd2f4958bb3d7a1022072195c0209e
root <usman8806@yahoo.com>
2013-07-16 17:25:31 -0500

    Created view for options

--- txprintco/views/products/map.js
+++ txprintco/views/products/map.js
@@ -1,7 +1,8 @@
 function(doc) {
-  emit(doc.parent_cat.title, {
+  emit([doc.parent_cat.title, doc.subcat], {
   	'title': doc.title,
   	'base_price': doc.base_price,
-  	'product_id': doc._id
+  	'product_id': doc._id,
+  	'subcat': doc.subcat
   });
 }
--- txprintco/views/subcat_with_products/map.js
+++ txprintco/views/subcat_with_products/map.js
@@ -1,7 +1,10 @@
 function(doc) {
-  emit([doc.parent_cat.title, doc.subcat], {
-  	'title': doc.title,
-  	'base_price': doc.base_price,
-  	'product_id': doc._id
-  });
+//	var obj = {};
+//	obj[doc.subcat] = {'title': doc.title, 'base_price': doc.base_price, 'product_id': doc._id}
+//  emit(doc.parent_cat.title, {doc.subcat:
+//  	'title': doc.title,
+//  	'base_price': doc.base_price,
+//  	'product_id': doc._id
+//  });
+	emit(doc.subcat, {'product_id': doc._id});
 }
