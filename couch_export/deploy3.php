a99fc030b24be5cf18f8b3faf908952efb442ee8
Usman Tammana <usman8806@yahoo.com>
2013-08-22 12:00:35 -0500

    Added product_id: hash of the product_url

--- couch_export/deploy3.php
+++ couch_export/deploy3.php
@@ -8,7 +8,7 @@ $host = '127.0.0.1';
 $port = '5984';
 $user = 'root';
 $pass = 'xyz786';
-$db_name = 'tpc_product_documents';
+$db_name = 'tpc_product_documents_md5_product_test';
 
 
 $server = new SetteeServer('http://'.$user.':'.$pass.'@'.$host.':'.$port);
@@ -42,17 +42,21 @@ echo "Pushing CouchApp\n";
 exec("couchapp push ../txprintco http://".$user.":".$pass."@".$host.":".$port."/".$db_name);
 
 //$products = array();
-$base_path = '/home/usman/Developer/Crawls/CrawlDump_July18';
+$base_path = '/home/usman/Developer/Crawls/CrawlDump_July19';
 $data_set_1 = file_get_contents($base_path.'/part1/crawldump_0_to__i17_j54.tpd');
 $data_set_2 = file_get_contents($base_path.'/part2/crawldump_i17_j55_to_end_flyers_removeshortruns.tpd');
 $data_set_3 = file_get_contents($base_path.'/part3-posters/crawldump_jul-07-2013_R4OqVp_i18_poster_end.tpd');
 $data_set_4 = file_get_contents($base_path.'/part4/crawldump_jul-15-2013_RJIp0o.tpd');
 $data_set_5 = file_get_contents($base_path.'/part5/crawldump_jul-17-2013_jqGoNI.tpd');
+$data_set_6 = file_get_contents($base_path.'/part6/crawldump_jul-18-2013_aI8RWI.tpd');
+
 
 $data_1 = igbinary_unserialize($data_set_1);
 $data_2 = igbinary_unserialize($data_set_2);
 $data_3 =  igbinary_unserialize($data_set_3);
 $data_4 =  igbinary_unserialize($data_set_4);
+$data_5 =  igbinary_unserialize($data_set_5);
+$data_6 =  igbinary_unserialize($data_set_6);
 
 //$products_types = array_merge_recursive($data_1, $data_2, $data_3);
 
@@ -60,6 +64,8 @@ $data_4 =  igbinary_unserialize($data_set_4);
 megeArrays($data_1, $data_2);
 megeArrays($data_1, $data_3);
 megeArrays($data_1, $data_4);
+megeArrays($data_1, $data_5);
+megeArrays($data_1, $data_6);
 
 $products_types = $data_1;
 
@@ -108,7 +114,7 @@ foreach($products_types as $product_cat_id => $product_cat) {
 			{
 				// var_dump($product_id);
 				$products_types[$product_cat_id]['products'][$product_id]['parent_cat'] = array('title' => $product_cat['title'],  'url' => $product_cat['url']);
-
+				$products_types[$product_cat_id]['products'][$product_id]['product_id'] = md5($product_cat['url']);
 				//Add To couch
 				// var_dump($products_types[$product_cat_id]['products'][$product_id]);
 				$doc = $products_types[$product_cat_id]['products'][$product_id];
--- txprintco/views/options-object/map.js
+++ txprintco/views/options-object/map.js
@@ -1,3 +1,11 @@
 function(doc) {
-  
+	for(i in doc.runsizes) {
+		for(j in doc.runsizes[i]["colors"]) {
+			for(k in doc.runsizes[i]["colors"][j]["tat"]){
+				var obj = {};
+				obj[k] = doc.runsizes[i]["colors"][j]["tat"][k]["price"]["options"];
+				emit([doc._id, i, j, k], obj);
+			}
+		}
+	}
 }
