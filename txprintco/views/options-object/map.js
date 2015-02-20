fdb57c05d47efbefd693f050637f56d9eef46de9
Asad Hasan <asad@dadesigners.com>
2015-02-19 21:51:31 -0600

    Revamp options-object array.

--- txprintco/views/options-object/map.js
+++ txprintco/views/options-object/map.js
@@ -3,10 +3,10 @@ function(doc) {
 		for(i in doc.runsizes) {
 			for(j in doc.runsizes[i]["colors"]) {
 				for(k in doc.runsizes[i]["colors"][j]["tat"]){
-					var obj = {};
-					obj[k] = doc.runsizes[i]["colors"][j]["tat"][k]["price"]["options"];
-					emit([doc.product_id, i, j, k], obj);
-					//emit([doc.product_id, i, j, k], doc.product_id);
+					var options = doc.runsizes[i]["colors"][j]["tat"][k]["price"]["options"];
+					for(l in options) {
+						emit([doc.product_id, i, j, k, l], options);
+					}
 				}
 			}
 		}
