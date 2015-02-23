d7842f2ab2c534966f176a95e0d9fce82eabff01
Asad Hasan <asad@dadesigners.com>
2015-02-23 17:13:02 -0600

    Fix map after datachange

--- txprintco/views/filters-product-map/map.js
+++ txprintco/views/filters-product-map/map.js
@@ -3,7 +3,8 @@ function(doc) {
     for(i in doc.categories) {
 	var options = doc.categories[i]["options"];
 	for(j in options) {
-                emit([doc.product_type, i, j], options[j]);
+                emit([doc.product_type, doc.categories[i].vocabulary_machine_name
+, options[j].term_name], options[j].products);
 	}
     }
 
