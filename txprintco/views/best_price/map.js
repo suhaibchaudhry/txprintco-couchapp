77575592d51473210d9da7d842587912ba83728b
Asad Hasan <asad@dadesigners.com>
2015-06-01 16:56:34 -0500

    Strip symbols from price attributes.

--- txprintco/views/best_price/map.js
+++ txprintco/views/best_price/map.js
@@ -6,6 +6,7 @@ function(doc) {
 				for(k in doc.runsizes[i]["colors"][j]["tat"]){
 					var obj = {};
 					obj[k] = doc.runsizes[i]["colors"][j]["tat"][k]["price"]["regular"];
+					obj[k].base_price = obj[k].base_price.replace(/[^0-9\.]+/g,"");
 					emit([doc.product_id, i, j], obj);
 				}
 				// emit([doc._id, i, j], obj);
