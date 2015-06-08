e70e25e850f6761d5a7455ffddad49f217eb7e97
Asad Hasan <asad@dadesigners.com>
2015-06-08 10:27:39 -0500

    Update stage.

--- couch_export/deploy.php
+++ couch_export/deploy.php
@@ -9,7 +9,7 @@ $host = '127.0.0.1';
 $port = '5984';
 $user = 'root';
 $pass = 'xyz786';
-$db_name = 'txprintco_dev_stage7';
+$db_name = 'txprintco_dev_stage8';
 
 
 $server = new SetteeServer('http://'.$user.':'.$pass.'@'.$host.':'.$port);
