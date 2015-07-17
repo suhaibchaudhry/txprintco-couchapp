07ca4c3aec38ec4033c73e6c6749045c66f45ce9
Asad Hasan <asad@dadesigners.com>
2015-07-17 11:35:48 -0500

    Replace category id in keys with uri

--- txprintco/views/products/map.js
+++ txprintco/views/products/map.js
@@ -1,6 +1,6 @@
 function(doc) {
   if(doc.object_type === 'product') {
-	  emit([doc.parent_cat.title, doc.subcat], {
+	  emit([doc.parent_cat.title.toLowerCase().replace(/[_\W]+/g, '-'), doc.subcat], {
 	  	'title': doc.title,
 	  	'base_price': doc.base_price,
 	  	'product_id': doc.product_id,
