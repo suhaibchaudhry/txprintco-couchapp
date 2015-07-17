9bc3b89e0022c3549043b712a381d8dcc46a4dc3
Asad Hasan <asad@dadesigners.com>
2015-07-16 19:09:14 -0500

    Make inline.

--- txprintco/views/categories/map.js
+++ txprintco/views/categories/map.js
@@ -1,9 +1,5 @@
 function(doc) {
   if(doc.object_type === 'product') {
-  	emit([doc.product_type_weight, generate_object_uri(generate_object_uri(doc.parent_cat.title))], doc.parent_cat.title);
+  	emit([doc.product_type_weight, doc.parent_cat.title.toLowerCase().replace(/[_\W]+/g, '-')], doc.parent_cat.title);
   }
-}
-
-function generate_object_uri(identifier) {
-	return identifier.toLowerCase().replace(/[_\W]+/g, '-');
 }
