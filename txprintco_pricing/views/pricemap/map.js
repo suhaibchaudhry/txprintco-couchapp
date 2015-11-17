c1d17441bd27e0e3773811838f604f7f37590a01
Asad Hasan <asad@dadesigners.com>
2015-11-17 14:28:55 -0600

    Added price override map function

--- txprintco_pricing/views/pricemap/map.js
+++ txprintco_pricing/views/pricemap/map.js
@@ -1,3 +1,14 @@
 function(doc) {
-  
+  var key = [];
+  key.push(doc.activeKey.product_id);
+  if(doc.activeKey.runsize) {
+  	key.push(doc.activeKey.runsize);
+  }
+  if(doc.activeKey.color) {
+  	key.push(doc.activeKey.color);
+  }
+  if(doc.activeKey.tat) {
+  	key.push(doc.activeKey.tat);
+  }
+  emit(key, doc.price);
 }
