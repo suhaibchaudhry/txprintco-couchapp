6ee1ac9bdb28f04839d759220e315cd43d1dfc67
Asad Hasan <asad@dadesigners.com>
2015-11-17 16:12:26 -0600

    Add reduce.

---
+++ txprintco_pricing/views/pricemap/reduce.js
@@ -0,0 +1,13 @@
+function(keys, values, rereduce) {
+    var amount = 0;
+    var best_price = '';
+    for(var i in values) {
+      var value = Number(values[i]);
+      if(value < amount || amount == 0) {
+        amount = value;
+        best_price = values[i];
+      }
+    }
+
+    return best_price;
+}
